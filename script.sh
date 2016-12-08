26fae2fdc30e0e67e37be76eee0464c9dfcd25ee
TeaTracer <teatracer7@yandex.ru>
2016-12-08 23:09:59 +0300

    Add first scripts for deploying to Vagrant.

---
+++ script.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+LOG="/var/log/app-deploy.log"
+PROJECT="django-test"
+BRANCH="dev" # master or dev
+REPOSITORY="git@github.com:TeaTracer/$PROJECT.git"
+
+teelog() {
+    echo $(date -u) "|" $1 | tee -a $LOG
+}
+
+teelog "Start deploying."
+teelog "Installing APT repositories."
+
+# PostgreSQL
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get install -y wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# RabbitMQ
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee -a /etc/apt/sources.list.d/rabbitmq.list
+wget --quiet -O - https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+
+# Custom soft
+sudo apt-get install -y htop vim tmux rsync ansible
+
+# PostgreSQL
+teelog "Installing PostgreSQL."
+sudo apt-get install -y postgresql-9.2 pgadmin3
+echo "postgres:postgres" | sudo chpasswd
+
+# RabbitMQ
+teelog "Installing RabbitMQ."
+sudo apt-get install -y rabbitmq-server
+
+# Git
+teelog "Downloading git project."
+git clone -b $BRANCH $REPOSITORY
+
+# Pip requirenments
+teelog "Installing requirenments."
+sudo apt-get install -y python3-pip
+(cd $PROJECT && sudo pip3 install -r requirenments.txt)
