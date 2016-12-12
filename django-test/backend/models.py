b78c2136869f21663721fdef838d789ff12c44e1
TeaTracer <teatracer7@yandex.ru>
2016-12-13 00:28:42 +0300

    Store random Arrayfields to Postgres. Show them.

--- django-test/backend/models.py
+++ django-test/backend/models.py
@@ -1,7 +1,10 @@
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-class Data(models.Model):
-    data = JSONField()
-    data_date = models.DateTimeField('Date of dataset.')
+class Dataset(models.Model):
+    data = ArrayField(ArrayField(models.IntegerField()))
+    data_date = models.DateTimeField('Date of dataset.', auto_now_add=True)
+
+    def __str__(self):
+        return str(self.data)
--- django-test/backend/views.py
+++ django-test/backend/views.py
@@ -1,5 +1,29 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.template import loader
+from .models import Dataset
+
+#  def index(request):
+    #  return HttpResponse("This is app view response.")
 
 def index(request):
-    return HttpResponse("This is app view response.")
+    datasets = Dataset.objects.order_by('-data_date')
+    template = loader.get_template('index.html')
+    context = {
+        'datasets': datasets,
+    }
+    return HttpResponse(template.render(context, request))
+
+def dataset_view(request, dataset_id):
+    dataset = Dataset.objects.get(pk=dataset_id)
+    return HttpResponse(dataset)
+
+def add_random_dataset(request):
+    r = lambda : random.randint(10, 30)
+    dataset_length = r()
+    dataset_list = [[r(), r()] for _ in range(dataset_length)]
+    dataset = Dataset.objects.create(data=dataset_list)
+    return HttpResponse(dataset)
+
